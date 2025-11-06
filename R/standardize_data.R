6bb50f4dcce092f4042e580197fb6f7bea11fa86
leroy-bml <benjamin.leroy13@gmail.com>
2025-11-07 00:15:35 +0100

    add orchestrator for post-processing (after mapping)

---
+++ R/standardize_data.R
@@ -0,0 +1,61 @@
+#'
+#'
+#'
+
+standardize_data <- function(dataset, data_model = c("icasa", "dssat")) {
+  
+  #MATCH ARGS
+  
+  switch(
+    data_model,
+    "icasa" = {
+      out <- .standardize_icasa_data(dataset)
+    },
+    "dssat" = {
+      out <- .standardize_dssat_data(dataset)
+    }
+  )
+  
+  return(out)
+}
+
+#'
+#'
+#'
+
+# TODO TEST: whole routine should be robust to missing SOIL/WEATHER DATA
+.standardize_dssat_data <- function(dataset, build = TRUE) {
+
+  # Apply DSSAT standard codes
+  dataset_nms <- apply_naming_rules(dataset)
+  # TODO: test with single exp + add file names
+  
+  # Split by experiment (as per DSSAT definition)
+  dataset_split <- split_dataset(dataset_nms, key = "experiment", data_model = "dssat")
+  # Split weather data to match the cultivation season for each experiment
+  dataset_split <- lapply(dataset_split, extract_season_weather)
+  
+  # Extract comments
+  comments_split <- lapply(dataset_split, function(ls) {
+    comments <- purrr::map(dataset, extract_dssat_notes)
+    keep(comments, ~ nrow(.x) > 0)
+  } )
+  
+  # Apply required object structure for file export with DSSAT library functions
+  dataset_split_fmt <- purrr::map2(
+    dataset_split,
+    comments_split,
+    format_dssat_sections,
+    build = build
+  )
+
+  return(dataset_split_fmt)
+}
+
+
+#'
+#'
+#'
+
+# TODO
+.standardize_icasa_data <- function()
